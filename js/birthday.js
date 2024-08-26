a4b1ead8e33608d7f2c9914368aca8b0a6f60707
swam97 <ammarsafwan.wahid@gmail.com>
2024-08-26 23:12:30 +0800

    enable dom listening

--- js/birthday.js
+++ js/birthday.js
@@ -1,12 +1,16 @@
-const muteButton = document.getElementById('mute-button');
-const audioElement = document.getElementById('background-music');
+document.addEventListener('DOMContentLoaded', () => {
 
-muteButton.addEventListener('click', () => {
-    if (audioElement.muted) {
-        audioElement.muted = false;
-        muteButton.textContent = 'Mute'; // Update button text to 'Mute'
-    } else {
-        audioElement.muted = true;
-        muteButton.textContent = 'Unmute'; // Update button text to 'Unmute'
-    }
+    const muteButton = document.getElementById('mute-button');
+    const audioElement = document.getElementById('background-music');
+
+    muteButton.addEventListener('click', () => {
+        if (audioElement.muted) {
+            audioElement.muted = false;
+            muteButton.textContent = 'Mute'; // Update button text to 'Mute'
+        } else {
+            audioElement.muted = true;
+            muteButton.textContent = 'Unmute'; // Update button text to 'Unmute'
+        }
+    });
+
 });
