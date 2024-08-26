7f388168e0230a3c1e813a8eaecda12b35ab491f
swam97 <ammarsafwan.wahid@gmail.com>
2024-08-26 22:31:20 +0800

    add background music and candle

---
+++ js/birthday.js
@@ -0,0 +1,12 @@
+const muteButton = document.getElementById('mute-button');
+const audioElement = document.getElementById('background-music');
+
+muteButton.addEventListener('click', () => {
+    if (audioElement.muted) {
+        audioElement.muted = false;
+        muteButton.textContent = 'Mute'; // Update button text to 'Mute'
+    } else {
+        audioElement.muted = true;
+        muteButton.textContent = 'Unmute'; // Update button text to 'Unmute'
+    }
+});
